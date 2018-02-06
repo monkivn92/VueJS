7d49f97d14ac3c1cbdef014b141c31f17dcbcbfe
vuongpv <monkivn92@gmail.com>
2018-02-06 15:23:56 +0700

    first commit

---
+++ ChildComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>The precious thing: {{myProp}}</p>
+</template>
+
+<script type="module">
+    export default {
+        props: [
+            'myProp'
+        ]
+    }
+</script>
---
+++ ParentComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <child-component :myProp="preciousThing"></child-component>
+</template>
+
+<script type="module">
+import ChildComponent from 'ChildComponent.vue';
+
+export default 
+{
+  components: {
+    ChildComponent
+  },
+
+  data() {
+    return {
+      preciousThing: 'ring'
+    }
+  }
+}
+</script>
